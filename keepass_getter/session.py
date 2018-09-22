37a6bc9737dc7d17533f041496da4d9ee36c1982
MGregoire <gregoire@martinache.net>
2018-09-22 10:16:32 +0200

    Fix first association bug

--- keepass_getter/session.py
+++ keepass_getter/session.py
@@ -16,16 +16,15 @@ class Session(object):
         config.read(path)
         id_ = config['Session']['ID']
         key = config['Session']['key']
-        key_byte = key.encode('utf-8')
         requestor = protocol.Requestor(config['KeepassHTTP']['url'])
         if not id_ and not key:
             print("No previous association. Loading new association")
             key, id_ = requestor.getAndSaveNewAssociation(config)
-        elif not protocol.testAssociate(id_, key_byte, requestor):
+        elif not protocol.testAssociate(id_, key, requestor):
             print("Previous association failed. Loading new association")
             key, id_ = requestor.getAndSaveNewAssociation(config)
 
-        return self(key_byte, id_, requestor)
+        return self(key, id_, requestor)
 
     def getLogins(self, url):
         return protocol.getLogins(url, self.id_, self.key, self.requestor)
